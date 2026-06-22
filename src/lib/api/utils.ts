d0ac02e85a2a00334915d4a555cb2976a31aa025
VisShon <vshon447@gmail.com>
2026-06-22 18:06:08 +0530

    updated cookie auth

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -72,16 +72,13 @@ const _request = async <T>(
         }
 
         if (withAuth) {
-            // const cookies = await chrome?.cookies?.getAll({
-            //     url: import.meta.env.VITE_API_URL,
-            // })
+            const cookies = await chrome?.cookies?.getAll({
+                url: import.meta.env.VITE_API_URL,
+            })
 
-            // headers["Cookie"] = cookies
-            //     .map(cookie => `${cookie.name}=${cookie.value}`)
-            //     .join("; ")
-
-            headers["Cookie"] = 
-                "sendin_auth=MTc4MTI5NjI4MHxkVGhlZzhBNUdQdzlZZjg2eDBkNS1pbkUyUnhHWThNVEd2bTVXODhyT3RrNnRjZDVIaS1QWFdKNUVwT1laMjRlVlMxUnZoTFo4RzlsY0htX0V1b3JkcXQ0a25kdHd1Wi1YdHZpWWtheng0Y2FoclZ5cVJJYlA1blIwSU0wSVdZRExwUU1iUT09fJT5Iw0Rszpt-dalKKvDC6-l2zo5rF8BZBERtieEX7nZ"
+            headers["Cookie"] = cookies
+                .map(cookie => `${cookie.name}=${cookie.value}`)
+                .join("; ")
         }
 
         const request = await fetch(
